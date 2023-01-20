ace1ba89dc0bedff2913efcb0d0367635c61da1b
NORWYS LEON <norwysleon@gmail.com>
2023-01-20 15:59:17 -0300

    6t0 commit: login - logout, autenticaciones

--- AppBlog/urls.py
+++ AppBlog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 from django.contrib.auth.views import LogoutView
 
+
 urlpatterns = [
     path('', inicio, name="inicio"),
     path('acercademi/', acercademi, name="acercademi"),
@@ -18,5 +19,8 @@ urlpatterns = [
     path('blog/list/', BlogList.as_view(), name="blog_list"),
     path('blog/<pk>', BlogDetalle.as_view(), name="blog_detalle"),
 
+    path('register/', register, name="register"),
+    path('login/', login_usuario, name="login"), #No se puede llamar solo login porque ya existe una funcion login por eso login_request, puede tener cualquier nombre.
+    path('logout/', LogoutView.as_view() , name="logout"),
     
 ]
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -2,28 +2,39 @@ from django.shortcuts import render
 from .models import Blog
 from django.http import HttpResponse
 
-from AppBlog.forms import BlogForm
+from AppBlog.forms import BlogForm, RegistroUsuarioForm
 
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate
+
+from django.contrib.auth.decorators import login_required  #para vistas basadas en funciones def
+from django.contrib.auth.mixins import LoginRequiredMixin #para vistas basadas en class
+
 
 # Create your views here.
 
 
-
+@login_required
 def blog(request):
     blogues= Blog(titulo= "Python", subtitulo="Lenguaje", cuerpo="xxxxxx", fecha="2023-01-20", autor="", imagen="")
     blogues.save()
     cadena_texto=f"Blog guardado"
     return HttpResponse(cadena_texto)
 
+
+@login_required
 def blogs(request):
     return render(request, "blogs.html")
+
 
 def inicio(request):
     return render(request, "inicio.html")
 
+
 def acercademi(request):
     return render(request, "acercademi.html")
+
 
 #Este se usa cuando se realiza el formulario en el html
 """def blogFormulario(request):
@@ -40,7 +51,7 @@ def acercademi(request):
     else:
         return render(request, "blogFormulario.html")"""
 
-
+@login_required
 def blogFormulario(request):
     if request.method=="POST":
         form=BlogForm(request.POST)
@@ -60,10 +71,11 @@ def blogFormulario(request):
         formulario=BlogForm()
         return render(request, "blogFormulario.html", {"form": formulario, "mensaje": "Información no valida"})
 
-
+@login_required
 def busquedaTitulo(requets):
     return render(requets, "busquedaTitulo.html")
 
+@login_required
 def buscar(request):
     titulo=request.GET["titulo"]
     if titulo!="":
@@ -72,18 +84,19 @@ def buscar(request):
     else:
         return render (request, "busquedaTitulo.html", {"mensaje": "Favor ingresar un titulo para buscar"})
 
-
+@login_required
 def leerBlogs(request):
     blogs=Blog.objects.all()
     return render (request, "leerBlogs.html", {"blogs":blogs})
 
+@login_required
 def eliminarBlog(request, id):
     blog= Blog.objects.get(id=id) #Trae los datos del model Blog que concuerda con el id. como es get es uno solo.
     blog.delete()
     blogs=Blog.objects.all()
     return render(request, "leerBlogs.html", {"blogs":blogs, "mensaje": "Blog eliminado correctamente"})
 
-
+@login_required
 def editarBlog(request, id):
     blog=Blog.objects.get(id=id)
     if request.method=="POST":
@@ -104,12 +117,52 @@ def editarBlog(request, id):
         formulario= BlogForm(initial={"titulo":blog.titulo, "subtitulo":blog.subtitulo, "cuerpo":blog.cuerpo, "fecha":blog.fecha, "autor":blog.autor, "imagen":blog.imagen})  #Trae los datos iniciales del Blog
         return render (request, "editarBlog.html", {"form": formulario})
 
+
 #Permite listar todos los blogs de la BD, con información mínima de dicho blog.
-class BlogList(ListView):
+class BlogList(LoginRequiredMixin, ListView):
     model=Blog
     template_name= "BlogList.html"
 
+
 #Al clickear en "Leer más" navega al detalle de cada blog mediante un 'blog/<pk>'.
-class BlogDetalle(DetailView):
+class BlogDetalle(LoginRequiredMixin, DetailView):
     model=Blog
     template_name="BlogDetalle.html"
+
+
+#vista de registro de usuario
+
+def register(request):
+    if request.method=="POST":
+        form=RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get("username")
+            form.save()
+            return render (request, "inicio.html", {"mensaje": f"Usuario {username} fue creado exitosamente.!"})      
+        else:
+            return render (request, "register.html", {"form": form, "mensaje": "ERROR AL CREAR USUARIO!"})  
+    else:
+        form=RegistroUsuarioForm()
+        return render (request, "register.html", {"form": form})
+
+
+#vista de login
+
+def login_usuario(request):
+    if request.method=="POST":
+        form=AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            info=form.cleaned_data
+            usu=info["username"]
+            contraseña=info["password"]
+            usuario=authenticate(username=usu, password=contraseña)
+            if usuario is not None:
+                login(request, usuario)
+                return render (request, "inicio.html", {"mensaje":f"Usuario {usu} logueado correctamemte"})
+            else:
+                return render(request, "login.html", {"form": form, "mensaje": "Usuario y/o contraseña incorrectos"})
+        else:
+            return render(request, "login.html", {"form": form, "mensaje": "Usuario  y/o contraseña incorrectos"})
+    else:
+        form=AuthenticationForm()
+        return render (request,"login.html", {"form": form})
